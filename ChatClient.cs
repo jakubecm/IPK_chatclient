da43aea90333f5db780c4c729795051dc9a8f882
Milan Jakubec <jakubeclp@gmail.com>
2024-05-20 02:27:41 +0200

    docs: documented ChatClient code

--- ChatClient.cs
+++ ChatClient.cs
@@ -1,26 +1,60 @@
+// -----------------------------------------------------------------------------
+// Project: IPK24ChatClient
+// File: ChatClient.cs
+// Author: Milan Jakubec (xjakub41)
+// Date: 2024-03-26
+// License: GPU General Public License v3.0
+// Description: This is implementation of chat client logic.
+// -----------------------------------------------------------------------------
 namespace IPK24ChatClient
 {
+    /// <summary>
+    /// Represents the state of the client.
+    /// </summary>
     public enum ClientState
     {
-        Start,
-        Auth,
-        Open,
-        Error,
-        End
+        Start, // Initial state before sending auth
+        Auth, // After sending auth and waiting for reply or in case of unsuccessful auth
+        Open, // After successful auth or join
+        Error, // In case of an error
+        End // After sending BYE or receiving BYE
     }
     public class ChatClient
     {
+        /// <value>ChatCommunicator instance used for communication with the server.</value>
         private readonly IChatCommunicator chatCommunicator;
+
+        /// <value>Server address.</value>
         private string serverAddress;
+
+        /// <value>Server port.</value>
         private int serverPort;
+
+        /// <value>Protocol used for communication with the server.</value>
         private string protocol;
+
+        /// <value>Current state of the client.</value>
         private ClientState clientState;
+
+        /// <value>Last command sent to the server.</value>
         private MessageType? lastCommandSent;
+
+        /// <value>Display name of the client.</value>
         public string? displayName;
+
+        /// <value>Channel ID of the client.</value>
         public string? channelId;
+
+        /// <value>Cancellation token source for the client used to abort operations.</value>
         private CancellationTokenSource cts;
+
+        /// <value>Dictionary of command handlers for handling user input.</value>
         private Dictionary<string, ICommandHandler> commandHandlers;
+
+        /// <value>Semaphore for ensuring only one command is executed at a time.</value>
         private SemaphoreSlim commandSemaphore = new SemaphoreSlim(1, 1);
+
+        /// <value>Task completion source for waiting for command completion.</value>
         public TaskCompletionSource<bool>? commandCompletionSource;
 
 
@@ -44,8 +78,12 @@ namespace IPK24ChatClient
             };
         }
 
+        /// <summary>
+        /// Runs the chat client and its methods.
+        /// </summary>
         public async Task RunAsync()
         {
+            // Handle Ctrl+C
             Console.CancelKeyPress += async (sender, e) =>
             {
                 e.Cancel = true; // Prevent the process from terminating.
@@ -56,20 +94,17 @@ namespace IPK24ChatClient
             try
             {
                 await chatCommunicator.ConnectAsync(serverAddress, serverPort);
-                Console.WriteLine("Connected to the server. You can start AUTH.");
                 var userInputTask = HandleUserInputAsync(cts.Token);
                 var listeningTask = ListenForMessagesAsync(cts.Token);
 
-                await Task.WhenAll(listeningTask, userInputTask);
+                await Task.WhenAll(listeningTask, userInputTask); // Wait for both tasks to finish
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine($"Error in run method: {ex.Message}");
+                Console.Error.WriteLine($"ERR: Run method error - {ex.Message}");
             }
             finally
             {
-                Console.WriteLine("Shutting down...");
-
                 if (chatCommunicator != null)
                 {
                     chatCommunicator.Disconnect();
@@ -79,6 +114,10 @@ namespace IPK24ChatClient
 
         }
 
+        /// <summary>
+        /// Listens for messages from the server and handles them accordingly.
+        /// </summary>
+        /// <param name="cancelToken">cancellation token that activates on ctrl+c press</param>
         private async Task ListenForMessagesAsync(CancellationToken cancelToken)
         {
             try
@@ -104,7 +143,7 @@ namespace IPK24ChatClient
                         else continue;
 
                     }
-                    else
+                    else // UDP listening
                     {
                         int udpTimeout = ((UdpChatCommunicator)chatCommunicator).getUdpTimeout();
                         var message = await chatCommunicator.ReceiveMessageAsync(udpTimeout);
@@ -124,6 +163,11 @@ namespace IPK24ChatClient
                 await SendBye();
             }
         }
+
+        /// <summary>
+        /// Handles the message based on its type.
+        /// </summary>
+        /// <param name="message">Message to be handled</param>
         private async void HandleMessageByType(Message message)
         {
             switch (message?.Type)
@@ -137,7 +181,7 @@ namespace IPK24ChatClient
                     {
                         await SendErrorMessage("Cannot receive messages in the current state.");
                         clientState = ClientState.Error;
-                        Console.Error.WriteLine($"ERR: Cannot receive messages in the current state.");
+                        Console.Error.WriteLine($"ERR: Received unexpected message.");
                         await SendBye();
                     }
                     break;
@@ -181,12 +225,11 @@ namespace IPK24ChatClient
                 case MessageType.Bye:
                     if (clientState != ClientState.Open)
                     {
-                        await SendErrorMessage("Unexpected BYE message.");
+                        await SendErrorMessage("Unexpected BYE message received.");
                         clientState = ClientState.Error;
                         Console.Error.WriteLine($"ERR: Unexpected BYE message received.");
                         await SendBye();
-                    }
-                    Console.WriteLine("Disconnected from the server.");
+                    }    
                     signalSemaphoreToRelease();
                     cts.Cancel();
                     clientState = ClientState.End;
@@ -203,6 +246,10 @@ namespace IPK24ChatClient
             }
         }
 
+        /// <summary>
+        /// Handles the reply message from the server.
+        /// </summary>
+        /// <param name="message">Message object to handle reply to</param>
         private void HandleReplyMessage(Message message)
         {
             if (message.ReplySuccess == true)
@@ -235,11 +282,15 @@ namespace IPK24ChatClient
         }
 
 
-
+        /// <summary>
+        /// Handles user input from the console.
+        /// </summary>
+        /// <param name="cancelToken">Cancel token for aborting operation</param>
         private async Task HandleUserInputAsync(CancellationToken cancelToken)
         {
             while (!cancelToken.IsCancellationRequested)
             {
+                // Check if input is redirected and there is no more input
                 if (Console.IsInputRedirected && Console.In.Peek() == -1)
                 {
                     clientState = ClientState.End;
@@ -247,6 +298,7 @@ namespace IPK24ChatClient
                     break;
                 }
 
+                // Check if there is any input available, either from the console or redirected input
                 bool inputAvailable = Console.IsInputRedirected ? Console.In.Peek() != -1 : Console.KeyAvailable;
 
                 if (inputAvailable)
@@ -261,8 +313,7 @@ namespace IPK24ChatClient
                             Console.Error.WriteLine("You must authenticate and join a channel before sending messages.");
                             continue;
                         }
-                        // TODO: Make a separate method for this?
-                        // Check if user input is max 1400 characters long and contains only printable characters with space (0x21 - 0x7E)
+                        // Check if user input is max 1400 characters long and contains only printable characters with space (0x20 - 0x7E)
                         if (userInput.Length > 1400)
                         {
                             Console.Error.WriteLine("Message is too long. Maximum length is 1400 characters.");
@@ -270,6 +321,7 @@ namespace IPK24ChatClient
                         }
                         foreach (char c in userInput)
                         {
+                            // 0x20 = space, 0x7E = ~
                             if (c < 0x20 || c > 0x7E)
                             {
                                 Console.Error.WriteLine("Message contains non-printable characters and can contain only printable characters and space.");
@@ -286,6 +338,7 @@ namespace IPK24ChatClient
                     var inputParts = userInput.Split(' ');
                     var command = inputParts[0].ToLower();
 
+                    // Try to find the command handler for the given command
                     if (commandHandlers.TryGetValue(command, out var handler))
                     {
                         if (handler.RequiresServerConfirmation)
@@ -299,7 +352,7 @@ namespace IPK24ChatClient
                             }
                             catch (OperationCanceledException)
                             {
-                                Console.Error.WriteLine("Command execution was canceled.");
+                                Console.Error.WriteLine("ERR: Command execution was canceled.");
                             }
                             finally
                             {
@@ -313,7 +366,7 @@ namespace IPK24ChatClient
                     }
                     else
                     {
-                        Console.Error.WriteLine($"Unknown command: {command}. Type '/help' for available commands.");
+                        Console.Error.WriteLine($"ERR: Unknown command: {command}. Type '/help' for available commands.");
                     }
                 }
                 else
@@ -331,42 +384,68 @@ namespace IPK24ChatClient
             }
         }
 
+        /// <summary>
+        /// Sends BYE message to the server and disconnects the client.
+        /// </summary>
         public async Task SendBye()
         {
             Console.WriteLine("Disconnecting...");
             Message byeMsg = new Message(MessageType.Bye);
             await chatCommunicator.SendMessageAsync(byeMsg);
             clientState = ClientState.End;
-            cts.Cancel();
+            cts.Cancel(); // Signal to input and listening tasks to stop
             chatCommunicator.Disconnect();
         }
 
+        /// <summary>
+        /// Sends an error message to the server.
+        /// </summary>
+        /// <param name="content">Content of the error</param>
         public async Task SendErrorMessage(string content)
         {
             Message errMsg = new Message(MessageType.Err, displayName: this.displayName, content: content);
             await chatCommunicator.SendMessageAsync(errMsg);
         }
 
+        /// <summary>
+        /// Sets the display name of the client.
+        /// </summary>
+        /// <param name="command">the message type to set the variable to</param>
         public void setLastCommandSent(MessageType command)
         {
             lastCommandSent = command;
         }
 
+        /// <summary>
+        /// Sets the display name of the client.
+        /// </summary>
+        /// <param name="id">the id to set</param>
         public void setChannelId(string id)
         {
             channelId = id;
         }
 
+        /// <summary>
+        /// Sets the display name of the client.
+        /// </summary>
+        /// <param name="state">the state to set</param>
         public void setClientState(ClientState state)
         {
             clientState = state;
         }
 
+        /// <summary>
+        /// Getter for the client state.
+        /// </summary>
+        /// <returns>the current client state</returns>
         public ClientState getClientState()
         {
             return clientState;
         }
 
+        /// <summary>
+        /// Signals the semaphore to release the lock.
+        /// </summary>
         public void signalSemaphoreToRelease()
         {
             // If something is waiting for command completion source, unlock it
